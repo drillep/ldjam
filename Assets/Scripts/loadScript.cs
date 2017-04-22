d9d32a8e5f455cd65f22d991bd7b156687693bd1
Gary Davies <garydavies@outlook.com>
2017-04-22 17:10:52 +0100

    load screen image github test

---
+++ Assets/Scripts/loadScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadScript : MonoBehaviour {
+
+    public float flashSpeed = 25f;
+    public Image loadingImage; 
+
+	// Use this for initialization
+	void Start () {
+
+        loadingImage.color = Color.Lerp(loadingImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
